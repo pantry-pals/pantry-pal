9f0d7ed8b31808e02aa28bca461c0c9b3e69bd32
Justin Smith <jws8@hawaii.edu>
2025-09-09 17:30:30 -1000

    fixed hero.tsx errors + linting errors

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,29 +1,32 @@
-"use client";
-import React from "react";
-import { useRouter } from "next/navigation";
+'use client';
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 export default function Hero() {
   const router = useRouter();
 
   const redirectToLogin = () => {
-    router.push("/auth/signin");
+    router.push('/auth/signin');
   };
 
   const redirectToSignup = () => {
-    router.push("/auth/signup");
+    router.push('/auth/signup');
   };
 
   return (
     <section className="container py-5">
-      <div style={{ marginTop: "75px" }} className="row align-items-center">
+      <div style={{ marginTop: '75px' }} className="row align-items-center">
 
         <div className="col-md-6 mb-4 mb-md-0 mt-5">
-          <div className="d-flex justify-content-center align-items-center" style={{ height: "300px" }}>
-            <img
+          <div className="d-flex justify-content-center align-items-center" style={{ height: '300px' }}>
+            <Image
               src="/PantryPalsLogo.png"
               alt="Pantry Pal Logo"
               width={500}
               height={500}
+              priority
             />
           </div>
         </div>
@@ -36,13 +39,15 @@ export default function Hero() {
             eum an brute copiosae hendrerit.
           </p>
           <div className="d-flex gap-2">
-            <button 
+            <button
+              type="button"
               className="btn btn-dark"
               onClick={redirectToLogin}
             >
               Log In
             </button>
             <button
+              type="button"
               className="btn btn-outline-dark"
               onClick={redirectToSignup}
             >
