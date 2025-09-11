0468d2b3c83f3bd68727caaa909badd80ef49095
pelitaf <pelita.felicitas@gmail.com>
2025-09-10 21:31:11 -1000

    created AboutUs Page (#78)
    About Us page created, linked in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,23 @@
-import { Col, Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 navandfooter">
     <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
-      </Col>
+      <Row>
+        <Col className="text-start">
+          Department of Information and Computer Sciences
+          <br />
+          University of Hawaii
+          <br />
+          Honolulu, HI 96822
+          <br />
+          <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
+        </Col>
+        <Col className="text-end">
+          <a href="/aboutus">About Us</a>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
