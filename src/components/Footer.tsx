aa2caf0a4ed12e01e7667ca3e2f82760f1cd4e21
Justin Smith <jws8@hawaii.edu>
2025-09-13 18:32:26 -1000

    footer component updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,34 @@
 import { Col, Container, Row } from 'react-bootstrap';
 
-/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 navandfooter">
     <Container>
       <Row>
         <Col className="text-start">
-          Department of Information and Computer Sciences
+          <strong>Pantry Pal</strong>
+          {' '}
+          &mdash; Your personal pantry manager
           <br />
-          University of Hawaii
-          <br />
-          Honolulu, HI 96822
-          <br />
-          <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
+          <a
+            href="https://github.com/pantry-pals"
+            style={{ color: 'var(--timberwolf)', textDecoration: 'underline' }}
+          >
+            View on GitHub
+          </a>
         </Col>
         <Col className="text-end">
-          <a href="/aboutus">About Us</a>
+          <a
+            href="/aboutus"
+            style={{ color: 'var(--timberwolf)', textDecoration: 'underline' }}
+          >
+            About Us
+          </a>
+          <br />
+          &copy;
+          {' '}
+          {new Date().getFullYear()}
+          {' '}
+          Pantry Pal Team
         </Col>
       </Row>
     </Container>
